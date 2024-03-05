3093a49ac927467b4e66365dae9b989f9c54a09c
Mayrani <160832253+MayLizValdez@users.noreply.github.com>
2024-03-04 22:37:10 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,118 +1,16 @@
 <?php
 # Maquillaje
 //1.Definir mis argumentos GET
-$maquillaje=$_GET("maquillajes");
+$maquillaje=$_GET("maquillaje");
 //2.Definir mi lista de elementos
-$unMaquillaje = [
-  [
-  "maquillaje" => "Blush liquido",
-  "marca" => "rare beauty",
-  "precio" => 879
-],
-   [
-  "maquillaje" => "Bronzer stick",
-  "marca" => "rare beauty",
-  "precio" => 1,099
-],
-   [
-  "maquillaje" => "lip oil",
-  "marca" => "rare beauty",
-  "precio" => 535
-],
-   [
-  "maquillaje" => "high lither",
-  "marca" => "rare beauty",
-  "precio" => 610
-],
-   [
-  "maquillaje" => "labial mate ",
-  "marca" => "rare beauty",
-  "precio" => 510
-],
-  
-];
-$maquillajes = [
-  $unMaquillaje
-];
-
-  [
-  "maquillaje" => "Lapiz Labial",
-  "marca" => "YSL",
-  "precio" => 204
-],
-   [
-  "maquillaje" => "Sombras Duolumier",
-  "marca" => "YSL",
-  "precio" => 505
-],
-   [
-  "maquillaje" => "Base Matte Miel",
-  "marca" => "YSL",
-  "precio" => 544
-],
-   [
-  "maquillaje" => "Paleta Multiusos",
-  "marca" => "YSL",
-  "precio" => 1,028
-],
-   [
-  "maquillaje" => "Fluid Shadow",
-  "marca" => "Dior Addict",
-  "precio" => 322
-],
-  
-];
-$maquillajes = [
-  $unMaquillaje
-];
- [
-  "maquillaje" => "Rubor liquido",
-  "marca" => "rare beauty",
-  "precio" => 839
-],
-   [
-  "maquillaje" => "lifther gloss",
-  "marca" => "maybeline",
-  "precio" => 189
-],
-   [
-  "maquillaje" => "blush liquido float on",
-  "marca" => "sheglam",
-  "precio" => 171
-],
-   [
-  "maquillaje" => "base perfection fundation",
-  "marca" => "sephora",
-  "precio" => 824
-],
-   [
-  "maquillaje" => "labial hidratante jelly wow",
-  "marca" => "sheglam",
-  "precio" => 167
-],
-   [
-  "maquillaje" => "lip oil",
-  "marca" => "dior",
-  "precio" => 880
-],
-   [
-  "maquillaje" => "fijador spray mini",
-  "marca" => "Mac",
-  "precio" => 560
-],
-   [
-  "maquillaje" => "Iluminador",
-  "marca" => "Mac",
-  "precio" => 230
-],
-   [
-  "maquillaje" => "Concealer",
-  "marca" => "rare beauty",
-  "precio" => 710
-],
-   [
-  "maquillaje" => "Liquid Highliter",
-  "marca" => "YSL",
-  "precio" => 780
-   
-  
+$SEPHORA = {
+  {
+  "maquillaje" => "terracotta blush"
+  "precio" => 990
+  },
+  {
+  "maquillaje" => "iluminator (iluminador liquido)"
+  "precios"=> 1,070
+  },
+{
+  "maquillaje" =>"
