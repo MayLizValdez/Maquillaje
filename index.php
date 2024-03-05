988c6402db8cdcc4177d5f9531155c9f9fb32da3
Mayrani <160832253+MayLizValdez@users.noreply.github.com>
2024-03-04 18:30:45 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,36 @@
+<?php
 # Maquillaje
 //1.Definir mis argumentos GET
 $maquillaje=$_GET("maquillaje");
 //2.Definir mi lista de elementos
-$maquillaje = [
-[
-"maquillaje" => "
+$unMaquillaje = [
+  [
+  "maquillaje" => "Blush liquido",
+  "marca" => "rare beauty",
+  "precio" => 879
+],
+   [
+  "maquillaje" => "Bronzer stick",
+  "marca" => "rare beauty",
+  "precio" => 1099
+],
+   [
+  "maquillaje" => "lip oil",
+  "marca" => "rare beauty",
+  "precio" => 535
+],
+   [
+  "maquillaje" => "high lither",
+  "marca" => "rare beauty",
+  "precio" => 610
+],
+   [
+  "maquillaje" => "labial mate ",
+  "marca" => "rare beauty",
+  "precio" => 510
+],
+  
+];
+$maquillajes = [
+  $unMaquillaje
+];
