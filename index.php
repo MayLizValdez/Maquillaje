2506bb93401bc01b43a0905aa0661d892ac681b6
Mayrani <160832253+MayLizValdez@users.noreply.github.com>
2024-03-05 18:37:28 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,15 +3,17 @@
 //1.Definir mis argumentos GET
 $maquillaje=$_GET("maquillaje");
 //2.Definir mi lista de elementos
-$Sheglam = [
-  
-  [
-  "maquillaje" => "Terracotta Blush"
-  "precio" => 990
-  ],
-  [
-  "maquillaje" => "Highlihter (Liquid Highlihter)"
-  "precios"=> 1,070
-  ],
-[
-  "maquillaje" => " 
+$Gloss = [
+  //Caracteristicas
+  "Marca" => "Sheglam",
+  "Precio"=> 65,
+  "Tono" => " Rojo (Frambuesa)",
+  "Textura" => " Liquido",
+  "Hecho en"=> "China",
+  "Ingrediemtes" => "Pigmentos, aceites, ceras vegetales y activos minerales o sintéticos",
+  "Tienda" => " Shein",
+  "Funcion" => " Dar acabado resplandeciente y, en algunos casos, agregar un delicado color",
+  "Reseña" => 4.8,
+  "Descripcion" => Brillo de labios de larga duracion hidratante con fruta",
+   ],
+ 
