eb772c60f7c32f76fcf0e2ab19e3b09db45b9016
Mayrani <160832253+MayLizValdez@users.noreply.github.com>
2024-03-05 16:40:57 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 //1.Definir mis argumentos GET
 $maquillaje=$_GET("maquillaje");
 //2.Definir mi lista de elementos
-$SEPHORA = {
-  {
+$Sheglam = [
+  
+  [
   "maquillaje" => "Terracotta Blush"
   "precio" => 990
-  },
-  {
+  ],
+  [
   "maquillaje" => "Highlihter (Liquid Highlihter)"
   "precios"=> 1,070
-  },
-{
-  "maquillaje" =>"
+  ],
+[
+  "maquillaje" => " 
